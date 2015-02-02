ccf579aeed54507f435c9591bf3f08fc73c7b07c
Matthew Coole <matthewcoole@gmail.com>
2015-02-02 15:09:02 +0000

    Adding visualisations

---
+++ Visualisations/Expenditure/chartVis.js
@@ -0,0 +1,28 @@
+function visChart(data, canvas) {
+    var chartData = [];
+    var pieOptions = {
+        segmentShowStroke: false,
+        animateScale: true
+    };
+    var results = data.results.bindings;
+    for (i = 0; i < results.length; i++) {
+        var unit = results[i].label.value;
+        var amount = parseFloat(results[i].count.value);
+        var col = getRandomColor();
+        chartData.push({value: amount, label: unit, color: col});
+    }
+    new Chart(canvas).Doughnut(chartData, pieOptions);
+}
+
+function getCanvas(canvasId) {
+    return $("#" + canvasId).get(0).getContext("2d");
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
