921ce3e4f9ff235025b5d72ba17a3a41a53fb7c2
Matt Coole <matthewcoole@gmail.com>
2015-02-10 16:45:41 +0000

    Added volunteering federated query

--- Visualisations/Locations/mapVis.js
+++ Visualisations/Locations/mapVis.js
@@ -11,61 +11,52 @@ function heatMap(data, map){
     var results = data.results.bindings;
     var items = [];
     for (i = 0; i < results.length; i++) {
-
-        var easting = parseFloat(results[i].easting.value);
-        var northing = parseFloat(results[i].northing.value);
-
-        var osr = new OSRef(easting, northing);
-
-        var ll = osr.toLatLng();
-
-        var gll = new google.maps.LatLng(ll.lat, ll.lng);
-
+		var gll = getLatLng(results[i]);
         items.push(gll);
     }
-
     var pointArray = new google.maps.MVCArray(items);
-
     var heatmap = new google.maps.visualization.HeatmapLayer({
         data: pointArray,
         radius: 20
     });
-
     heatmap.setMap(map);
+}
+
+function getLatLng(location){
+	var latLng;
+	if(location.hasOwnProperty("lat") && location.hasOwnProperty("long")){
+		latLng = new google.maps.LatLng(parseFloat(location.lat.value), parseFloat(location.long.value));
+	} else if(location.hasOwnProperty("northing") && location.hasOwnProperty("easting")){
+		var osr = new OSRef(parseFloat(location.easting.value), parseFloat(location.northing.value));
+		var ll = osr.toLatLng();
+		latLng = new google.maps.LatLng(ll.lat, ll.lng);
+	}
+	return latLng;
 }
 
 function visOnMap(data, map) {
     var results = data.results.bindings;
-    var items = [];
+	var infowindow = new google.maps.InfoWindow({
+		content: "temp"
+	});
     for (i = 0; i < results.length; i++) {
-        var loc = [results[i].type.value, parseFloat(results[i].long.value), parseFloat(results[i].lat.value), i];
-
-        items.push(loc);
-    }
-    setMarkers(map, items);
-}
-
-function setMarkers(map, locations) {
-    var shape = {
-        coords: [1, 1, 1, 20, 18, 20, 18, 1],
-        type: 'poly'
-    };
-    for (var i = 0; i < locations.length; i++) {
-        var item = locations[i];
-        var myLatLng = new google.maps.LatLng(item[1], item[2]);
-        var image = {
-            url: "images/" + item[0] + ".png",
-            size: new google.maps.Size(32, 32),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(16, 32)
-        };
-        var marker = new google.maps.Marker({
-            position: myLatLng,
+		var location = results[i];
+		var latLng = getLatLng(location);
+		var content = "<div>";
+		for(var key in location){
+			content += "<b>" + key + ":</b> " + location[key].value + "<br/>";
+		}
+		content += "</div>";
+		var marker = new google.maps.Marker({
+            position: latLng,
             map: map,
-            icon: image,
-            shape: shape,
-            title: item[0],
-            zIndex: item[3]
+            title: "test"
         });
+		google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
+			return function() {
+				infowindow.setContent(content);
+				infowindow.open(map,marker);
+			};
+		})(marker,content,infowindow));
     }
 }
